b3ccc1b61b0ae8a5fdee68e134409e07553687ec
Alfred Thormodseter <alfred.thormodseter@gmail.com>
2026-09-21 21:23:10 +0200

    Implementert ein front-end-mvp som køyrer parallelt med backend. Den vert ikkje brukt no, men skal integrerast litt og litt.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import StatusMessage from './components/StatusMessage'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>FangstPlot</h1>
+      <StatusMessage message="React fungerer" />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/StatusMessage.jsx
@@ -0,0 +1,5 @@
+function StatusMessage({ message }) {
+  return <div id="Status">{message}</div>
+}
+
+export default StatusMessage
